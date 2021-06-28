ecf7f417d042bb0eef71d4f25959fc6f8e27e83c
Jaeyoon <wodbs4338@naver.com>
2021-06-29 02:35:57 +0900

    zuul 사용해서 api gateway 구성중

---
+++ apigateway/src/test/java/yoon/toy/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package yoon.toy.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
