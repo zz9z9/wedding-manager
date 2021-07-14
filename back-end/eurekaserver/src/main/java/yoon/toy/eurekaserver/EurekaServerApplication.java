24ecd78d3ab7c4ac8bcc8c459a367869aa7aecff
Jaeyoon <wodbs4338@naver.com>
2021-07-14 17:31:32 +0900

    sub directory 커밋 테스트

--- back-end/eurekaserver/src/main/java/yoon/toy/eurekaserver/EurekaServerApplication.java
+++ back-end/eurekaserver/src/main/java/yoon/toy/eurekaserver/EurekaServerApplication.java
@@ -7,9 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
+    //
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
-
 }
